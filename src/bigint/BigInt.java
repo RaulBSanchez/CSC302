1fea2cb4713187dec49e256e6b74f758192324d3
Raul Sanchez <rsanchez@avanceon.com>
2019-02-07 11:50:18 -0500

    BigInt

---
+++ src/bigint/BigInt.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bigint;
+import java.math.BigInteger;
+
+/**
+ *
+ * @author rsanchez
+ */
+public class BigInt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         BigInteger bigFactorial = new BigInteger("1");
+        for(int i = 2; i <= 50; i++)
+        {
+           bigFactorial = bigFactorial.multiply(new BigInteger(i + ""));
+          
+           System.out.println(i + "! = " +bigFactorial);
+           
+        }
+    }
+    
+}
